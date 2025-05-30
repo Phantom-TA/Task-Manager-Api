e6f1fd38564e1d1ee7edba63c89bdc6906a8dca8
Tushar <tushar08032005@gmail.com>
2025-05-31 01:45:44 +0530

    first commit

---
+++ src/utils/mail.js
@@ -0,0 +1,78 @@
+import Mailgen from  "mailgen";
+import nodemailer from "nodemailer";
+
+const sendMail = async (options) => {
+      const mailGenerator = new Mailgen({
+      theme: "default",
+      product:{
+          name: "Task Manager",
+          link: "https://taskmanager.com",
+      },
+
+    })
+
+    var emailText = mailGenerator.generatePlaintext(options.mailgenContent);
+
+    var emailHtml = mailGenerator.generate(options.mailgenContent);
+
+    const transporter = nodemailer.createTransport({    
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false ,// true for 465, false for other ports
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+        },
+    });
+
+    const mail ={ 
+        from: 'mail.taskmanager@example.com',
+        to: options.email,
+        subject: options.subject,
+        text: emailText,
+        html: emailHtml,
+    }
+
+    try {
+        await transporter.sendMail(mail);
+    } catch (error) {
+        console.error("Error sending email:", error);
+    }
+}
+
+const emailVerificationMailGenContent = ( username ,
+     verificationurl) =>{
+         return {
+            body:{
+                name: username,
+                intro: "Welcome to Task Manager! We're excited to have you on board.",
+                action: {
+                    instructions: "To complete your registration, please verify your email address by clicking the button below.",
+                    button: {
+                        color: "#22BC66", // Optional action button color
+                        text: "Verify Email",
+                        link: verificationurl,
+                    },
+                },
+                outro : "Need help, or have questions? Just reply to this email, we\'d love to help.",
+            }
+         }
+}
+const forgotPasswordMailGenContent = ( username ,
+     passwordResetUrl) =>{
+         return {
+            body:{
+                name: username,
+                intro: "We received a request to reset your password for your Task Manager account.",
+                action: {
+                    instructions: "To reset your password , click the button below.",
+                    button: {
+                        color: "#22BC66", // Optional action button color
+                        text: "Reset Password",
+                        link: passwordResetUrl,
+                    },
+                },
+                outro : "Need help, or have questions? Just reply to this email, we\'d love to help.",
+            }
+         }
+}
